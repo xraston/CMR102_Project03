ddc8e1ce3cf55fdb11603e7da942f9e30d908456
xraston <1022147@student.sae.edu.au>
2020-12-10 15:45:59 +1100

    Scripts

---
+++ Assets/Scripts/ColourChange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour
+{
+    // public GameObject steakOutside;
+    // public GameObject steakInside;
+
+    Renderer m_Renderer;
+    private Color color = Color.red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+        m_Renderer.material.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeColour();
+    }
+
+    public void ChangeColour()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            color.r = 1f;
+            color.g = 1f;
+            color.b = 1f;
+            // color.a = 255;
+
+            m_Renderer.material.color = color;
+        }
+            
+    }
+    
+}
